05b88351c0a4681ce77343d1bd3e1ffae776ec5a
Lydia <lreitzel@gmail.com>
2021-07-15 16:10:22 -0400

    commit tree 96073870f702fb54d4c6796edc5c9f565d523762

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards (arrayNames, event) {
+    let cards = []
+    for (let i = 0; i < arrayNames.length; i++) {
+        cards.push(`Thank you, ${arrayNames[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards
+}
+
+function countDown (i) {
+    while (i > 0) {
+        console.log(i);
+        i -= 1;
+    }
+    console.log(i);
+}
